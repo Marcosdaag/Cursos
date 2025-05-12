1f082759fa97402a9e857f3d61830ca43d2f414e
Marcosdaag <marcosoffs99@gmail.com>
2025-05-12 20:03:31 -0300

    Operadores relacionales y condicional if

---
+++ Master en JavaScript/js/06condicionales.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/* 
+Operadores realcionales
+
+Mayor >
+Menor <
+Mayor o igual >=
+Menor o igual <=
+Igual ==
+Distinto !=
+*/
+
+// Los condicionales son estructuras de control que nos van a permitir hacer comprobaciones
+
+// Condicional IF
+
+var edad1 = 30;
+var edad2 = 12;
+
+//Si pasa esto
+if(edad1 > edad2){
+    //Ejecuta esto
+    console.log("Edad uno es mayor que edad dos");
+}else{
+    console.log("Edad uno es inferior");
+}
+
