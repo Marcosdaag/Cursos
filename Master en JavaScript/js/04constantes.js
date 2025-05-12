2978cae01638ae98188b9b2ab6bb6a36af31f26a
Marcosdaag <marcosoffs99@gmail.com>
2025-05-12 19:39:41 -0300

    Operadores y tipos de datos

--- Master en JavaScript/js/04constantes.js
+++ Master en JavaScript/js/04constantes.js
@@ -3,3 +3,10 @@
 // Las constantes son como variables pero su valor no puede cambiar
 
 var web = "google.com";
+const ip = "192.168.1.1";
+
+web = "facebook.com";
+// ip = "123" esto va a dar error ya que estoy intentando modificar una constante
+
+console.log(web, ip);
+
