6cbc48f3b321810c17b52e293cfc84767ad730fb
Marcosdaag <marcosoffs99@gmail.com>
2025-05-14 20:18:33 -0300

    Funciones

--- Master en JavaScript/js/19funciones.js
+++ Master en JavaScript/js/19funciones.js
@@ -2,3 +2,10 @@
 
 // Funciones, bloque de instrucciones que se ejecutan cada vez que se llama
 
+// Le definimos el nombre y la instruccion a ejecutar
+function calculadora(){
+    console.log("Hola");
+}
+
+// Invocar o llamar a la funcion
+calculadora();
