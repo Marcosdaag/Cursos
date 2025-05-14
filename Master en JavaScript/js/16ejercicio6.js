27c25552622561c2a38e93f3dc93494975eada38
Marcosdaag <marcosoffs99@gmail.com>
2025-05-14 15:53:03 -0300

    Termine el primer bloque de ejercicios

--- Master en JavaScript/js/16ejercicio6.js
+++ Master en JavaScript/js/16ejercicio6.js
@@ -1,2 +1,17 @@
 'use strict'
 
+/* 
+Que nos diga si un numero es par o impar.
+1 Ventana prompt
+2 Si no es valido el numero que nos lo pida de nuevo
+*/
+
+do {
+    var number = prompt(parseInt("Ingresa tu numero mayor a 0", 0));
+}while(isNaN(number || number<=0))
+
+if(number%2 != 0){
+    alert("El numero "+number+" es impar.");
+}else{
+    alert("El numero "+number+" es par.");
+}
