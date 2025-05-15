db4b64a1cf0182e9302fa3ad244da8c742ce2eef
Marcosdaag <marcosoffs99@gmail.com>
2025-05-15 13:27:07 -0300

    Funcion de callback

---
+++ Master en JavaScript/js/21funcionesanonimas.js
@@ -0,0 +1,24 @@
+'use strict'
+
+//Funciones anonimas
+//Funcion que no tiene nombre y puede ser guardado dentro de una variable y suele ser usado para hacer callbacks (funcion que se ejecuta dentro de otra)
+
+var pelicula = function(nombre){
+    return "La pelicula es "+nombre;
+}
+
+//Callbacks
+
+// Esta funcion tiene 3 parametros, el primer numero, el segundo numero y como tercer parametro es una funcion de callback que aun no esta definida pero al momento de llamar sumame podemos darle la logica que queramos
+function sumame(numero1, numero2, sumaPorDos){
+    var sumar = numero1+numero2;
+
+    sumaPorDos(sumar);
+
+    return sumar;
+}
+
+// En la funcion de arriba definimos que la funcion sumaPorDos recibe un dato que es sumar y despues cuando llamamos la funcion, definimos el comportamiento de esa funcion y que es lo que hace con ese dato
+sumame(9, 5, function(dato){
+    console.log("la suma por dos es "+dato*2 );
+})
