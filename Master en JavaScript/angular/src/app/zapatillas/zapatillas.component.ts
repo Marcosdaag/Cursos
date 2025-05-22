89f839c0b17224ccf6f95f557f7ce30d4246884a
Marcosdaag <marcosoffs99@gmail.com>
2025-05-22 16:27:17 -0300

    Termine con las directivas

--- Master en JavaScript/angular/src/app/zapatillas/zapatillas.component.ts
+++ Master en JavaScript/angular/src/app/zapatillas/zapatillas.component.ts
@@ -10,8 +10,12 @@ import { Zapatilla } from '../model/zapatilla';
 export class ZapatillasComponent implements OnInit{
     public titulo: string = "Componente de zapatillas";
     public zapatillas: Array<Zapatilla>;
+    public marcas: string[]; // Creo el array de marcas
+    public color: string;
 
 constructor(){
+  this.color = 'yellow';
+  this.marcas = new Array(); //Aca le doy un valor al array para incializarlo indefinido
   this.zapatillas = [ //Nuevo array de elementos Zapatilla dentro del array zapatillas
     new Zapatilla('Airforce','Nike','Blanco',12000, true),
     new Zapatilla('Runner MD','Nike','Negro',6000, true),
@@ -21,6 +25,17 @@ constructor(){
 
 ngOnInit(): void {
   console.log(this.zapatillas);
+
+  this.marcasList();
 }
 
+marcasList(){
+  this.zapatillas.forEach((zapatilla, index) =>{
+    //Vamos a crear una condicion para que las marcas no esten repetidas
+    if(this.marcas.indexOf(zapatilla.marca)<0){
+      this.marcas.push(zapatilla.marca);
+    }
+
+  });
+}
 }
