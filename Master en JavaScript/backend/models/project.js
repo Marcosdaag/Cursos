283e18a0ee9a19ab389d4ecf112e7d295188d0e4
Marcosdaag <marcosoffs99@gmail.com>
2025-06-04 16:08:14 -0300

    Guardar proyectos

--- Master en JavaScript/backend/models/project.js
+++ Master en JavaScript/backend/models/project.js
@@ -9,8 +9,9 @@ var ProjectSchema = Schema({
     name: String,
     description: String,
     category: String,
-    langs: [String],
-    year: Number
+    langs: String,
+    year: Number,
+    image: String
 });
 
 module.exports = mongoose.model('Project', ProjectSchema);
