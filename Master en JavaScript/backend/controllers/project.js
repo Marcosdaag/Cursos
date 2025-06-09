52e1cb4df98b6a106b216087732dd96439307d42
Marcosdaag <marcosoffs99@gmail.com>
2025-06-09 16:26:02 -0300

    Consulta de proyectos

--- Master en JavaScript/backend/controllers/project.js
+++ Master en JavaScript/backend/controllers/project.js
@@ -3,18 +3,21 @@
 var Project = require('../models/project');
 
 var controller = {
+    // Funcion home
     home: function (req, res) {
         return res.status(200).send({
             message: 'Soy la home por get'
         });
     },
 
+    // Funcion test
     test: function (req, res) {
         return res.status(200).send({
             message: 'Soy el test por post'
         });
     },
 
+    // Funcion para guardar
     saveProject: function (req, res) {
         var project = new Project();
 
@@ -40,6 +43,25 @@ var controller = {
                 if (error)
                     return res.status(500).send({ error: "Error al guardar el proyecto en la base de datos" });
             });
+    },
+
+    // Funcion getProject
+    getProject: function(req, res){
+        var projectId = req.params.id;
+
+        if(projectId == null){
+            return res.status(404).send({message: 'El proyecto no existe'});
+        }
+
+        Project.findById(projectId, (err, project) =>{
+
+            if(err) return res.status(500).send({message: 'Error al devolver los datos'});
+
+            if(!project) return res.status(404).send({message: 'El proyecto no existe'});
+
+            return res.status(200).send({project});
+
+        });
     }
 
 };
