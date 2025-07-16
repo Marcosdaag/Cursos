c5740eeee997dc6a7df84be45f318f63a50ba167
Marcosdaag <marcosoffs99@gmail.com>
2025-07-16 15:53:57 -0300

    Configuracion de headers y cors

--- Master en JavaScript/backend/app.js
+++ Master en JavaScript/backend/app.js
@@ -14,6 +14,21 @@ app.use(bodyParser.urlencoded({extended: false})); // Configuracion necesaria pa
 app.use(bodyParser.json()); // Esto hace que todos los datos que me lleguen son pasados a json
 
 // Cors
+// Configurar cabeceras y cors
+
+app.use((req, res, next) => {
+
+res.header('Access-Control-Allow-Origin', '*');
+
+res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+
+res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+
+res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+next();
+
+});
 
 // Rutas
 /* app.get('/', (request, response) =>{
